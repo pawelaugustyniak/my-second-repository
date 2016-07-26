a9970a75ade3d78224cc5283a0ecdc4b75e4ed1a
Augustyniak Pawel <rb21776G@raiffeisen.pl>
2016-07-26 11:32:53 +0200

    Initialize project

---
+++ src/main/java/pl/raiffeisen/HelloWorld.java
@@ -0,0 +1,10 @@
+package pl.raiffeisen;
+
+/**
+ * Created by rb21776 on 2016-07-26.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
